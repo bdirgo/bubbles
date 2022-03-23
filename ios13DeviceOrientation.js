0688580321fd00159753c029fe0fb33a0d908099
Benjamin Dirgo <bdirgo@gmail.com>
2022-03-23 10:57:47 -0700

    device orientation

---
+++ ios13DeviceOrientation.js
@@ -0,0 +1,31 @@
+function requestPermission() {
+    DeviceOrientationEvent.requestPermission()
+      .then(response => {
+        if (response === 'granted') {
+          permissionGranted = true;
+        } else {
+          permissionGranted = false;
+        }
+      })
+      .catch(console.error);
+      this.remove();
+}
+function ios13DeviceOrientation() {
+  if (typeof(DeviceOrientationEvent) !== undefined && typeof(DeviceOrientationEvent.requestPermission) === 'function') {
+    // iOS 13+
+    DeviceOrientationEvent.requestPermission()
+      .catch(() => {
+        // Only show permission dialog on first load, the browser will remember request after "allow"-ing
+        let button = createButton("Click to Allow sensor access, please");
+        // button.style('font-size', "42px");
+        button.center();
+        button.mousePressed(requestPermission);
+        // Skip "then"
+        throw error;
+      })
+      .then(() => {
+        // Already granted permission
+        permissionGranted = true;
+      });
+  }
+}
